4a29bcbd588f4e86b5acc85620d5ab9002b8d432
Yurex12 <yusufekungomi@gmail.com>
2023-12-18 08:50:25 +0100

    added sort by completion feature

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,25 +5,27 @@ function TaskList() {
   const { tasks, sortByValue } = useTodos();
 
   let sortedTask;
- // console.log(tasks)
 
   if (sortByValue === 'modified') {
     sortedTask = tasks;
   }
   if (sortByValue === 'alphabet') {
     sortedTask = tasks.slice().sort((a, b) => a.task.localeCompare(b.task));
-    console.log(tasks, sortedTask)
   }
 
-  // if (sortByValue === 'modified') {
-  //   sortedTask = tasks;
-  // } else if (sortByValue === 'alphabet') {
-  //   sortedTask = tasks.slice().sort((a, b) => a.task.localeCompare(b.task));
-  // }
-
+  if (sortByValue === 'completed-t') {
+    sortedTask = tasks
+      .slice()
+      .sort((a, b) => Number(b.completed) - Number(a.completed));
+  }
+  if (sortByValue === 'completed-b') {
+    sortedTask = tasks
+      .slice()
+      .sort((a, b) => Number(a.completed) - Number(b.completed));
+  }
 
   if (sortedTask.length === 0) {
-    return <p>Start by adding a new task</p>
+    return <p>Start by adding a new task</p>;
   }
 
   return (
@@ -32,7 +34,6 @@ function TaskList() {
       <ul className='space-y-5 mt-4'>
         {sortedTask?.map((task) => (
           <Task key={task.id} task={task} />
-          
         ))}
       </ul>
     </>
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+  return <div className='w-screen h-screen border-4 border-red-500 flex flex-col'>
+    
+    <div className="border-4 w-10/12 mx-auto  h-40 border-green-500"></div>
+    <div className="border-4 w-10/12 mx-auto  flex-1 border-blue-500"></div>
+  </div>;
+}
+
+export default Test;
